b628f5ca7dec3237770cca2cd860048426ea92c5
Sravan Bhamidipati <sbhamidipati@yext.com>
2022-07-11 17:03:20 -0400

    Add dist

---
+++ dist/es5/h5/dd-manager.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * dd-manager.ts 5.1.1
+ * Copyright (c) 2021 Alain Dumesny - see GridStack root license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DDManager = void 0;
+var DDManager = /** @class */ (function () {
+    function DDManager() {
+    }
+    return DDManager;
+}());
+exports.DDManager = DDManager;
+//# sourceMappingURL=dd-manager.js.map
